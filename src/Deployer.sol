9d59a290da1e530d30c0ddcaf85f72016e2d763e
Joshua Trujillo <Jtriley15@gmail.com>
2023-02-04 20:34:32 -0800

    update name

--- src/Deployer.sol
+++ src/Deployer.sol
@@ -7,7 +7,7 @@ using { appendArg, create } for bytes;
 
 bytes constant compiled = hex"60208038033d393d517f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc55603e8060343d393df3363d3d373d3d363d7f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc545af43d6000803e610039573d6000fd5b3d6000f3";
 
-function useMvp(address implementation) returns (address) {
+function useMinimumViableProxy(address implementation) returns (address) {
     return compiled.appendArg(implementation).create({value: 0});
 }
 
